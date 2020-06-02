b8a5044fe3c4accbfe1631c0e3baf4200ced0b6c
lrlong <66316581+lrlong@users.noreply.github.com>
2020-06-02 16:27:44 -0400

    Update Location.java
    Added setters to Location class.

--- Location.java
+++ Location.java
@@ -15,16 +15,32 @@ public class Location {
 		return street;
 	}
 	
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	
 	public String getCity() {
 		return city;
+	}
+	
+	public void setCity(String city) {
+		this.city = city;
 	}
 	
 	public String getState() {
 		return state;
 	}
 	
+	public void setState(String state) {
+		this.state = state;
+	}
+	
 	public int getZip() {
 		return zip;
+	}
+	
+	public void setZip(int zip) {
+		this.zip = zip;
 	}
 	
 	@Override
