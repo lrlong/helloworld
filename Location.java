9e07bae0eee69ea7e1c5747805a7a3faa3ea1ea6
lorin.long <lorin.long@tmctechnologies.com>
2020-06-02 11:23:48 -0400

    Adding New Class and other updates

---
+++ Location.java
@@ -0,0 +1,34 @@
+public class Location {
+	private String street;
+	private String city;
+	private String state;
+	private int zip;
+	
+	public Location(String street, String city, String state, int zip) {
+		this.street = street;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+	}
+	
+	public String getStreet() {
+		return street;
+	}
+	
+	public String getCity() {
+		return city;
+	}
+	
+	public String getState() {
+		return state;
+	}
+	
+	public int getZip() {
+		return zip;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("%s; %s, %s %s", street, city, state, zip);
+	}
+}
--- TestData.java
+++ TestData.java
@@ -1,9 +1,22 @@
 public class TestData {
-  private final int id;
-  private final String name;
+	private final int id;
+	private final String name;
   
-  public TestData(int id, String name) {
-    this.id = id;
-    this.name = name;
+	public TestData(int id, String name) {
+		this.id = id;
+		this.name = name;
   }
+  
+	public int getId() {
+		return id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	@Override
+	public String toString() {
+		return name;
+	}
 }
