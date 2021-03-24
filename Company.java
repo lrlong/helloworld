de7573a0dd82af837b75de23e48c0a62cd3256c6
lrlong <66316581+lrlong@users.noreply.github.com>
2021-03-24 14:26:54 -0400

    Update Company.java
    More Company updates

--- Company.java
+++ Company.java
@@ -1,8 +1,10 @@
 public class Company {
+  private long id;
   private String name;
   private String city;
   
-  public Company(String name, String city) {
+  public Company(long id, String name, String city) {
+      this.id = id;
       this.name = name;
       this.city = city;
   }
