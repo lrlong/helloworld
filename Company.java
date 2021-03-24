5b264bdc4c812f2484fcb0a584e86209474d585e
lrlong <66316581+lrlong@users.noreply.github.com>
2021-03-24 14:07:37 -0400

    Update Company.java
    Change to company

--- Company.java
+++ Company.java
@@ -1,7 +1,9 @@
 public class Company {
   private String name;
+  private String city;
   
-  public Company(String name) {
+  public Company(String name, String city) {
       this.name = name;
+      this.city = city;
   }
 }
