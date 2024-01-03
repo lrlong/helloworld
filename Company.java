bdb22c95a8bfa19f698b0e326ca58bcfb86f4b3c
lrlong <66316581+lrlong@users.noreply.github.com>
2024-01-03 16:12:28 -0500

    Update Company.java

--- Company.java
+++ Company.java
@@ -2,7 +2,10 @@ public class Company {
   private long id;
   private String name;
   private String city;
-  
+
+  /*
+  * This is the constructor.
+  */
   public Company(long id, String name, String city) {
       this.id = id;
       this.name = name;
